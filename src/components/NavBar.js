a3b8b029dc0a8b48107e28431c7ebf9f67cc75b4
karanja03 <wambuik03@gmail.com>
2023-07-25 16:22:57 +0300

    Complete react hooks router

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,49 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+
+  const linkStyles = {
+    display: "inline-block",
+    width: "50px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "blue",
+    textDecoration: "none",
+    color: "white",
+  };
+  return <div className="navbar">
+    <NavLink to="/movies"
+    style={linkStyles}
+    exact
+    activeStyle={{
+      background: "darkblue",
+    }}>Movies
+   </NavLink>
+   <NavLink to="/directors"
+    style={linkStyles}
+    exact
+    activeStyle={{
+      background: "darkblue",
+    }}>Directors
+   </NavLink>
+
+   <NavLink to="/actors"
+    style={linkStyles}
+    exact
+    activeStyle={{
+      background: "darkblue",
+    }}>Actors
+   </NavLink>
+
+   <NavLink to="/"
+    style={linkStyles}
+    exact
+    activeStyle={{
+      background: "darkblue",
+    }}>Home
+   </NavLink>
+
+  </div>;
 }
 
 export default NavBar;
